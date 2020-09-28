551c1aa0b9f065a3b52460441b2417189368f91c
“e <438238684@qq.com>
2020-09-28 17:34:47 +0800

    second commit

---
+++ Unity1/Assets/IronBars.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class IronBars : MonoBehaviour
+{
+    public float MaxY = 5.79f;
+    public float MinY = 4.06f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //print(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+   
+        if(Input.GetKey(KeyCode.C))
+        {
+            Lift();
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {   if (transform.position.y > MinY)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - 0.01f, transform.position.z);
+            }
+        }
+
+    }
+    public void Lift()
+    {
+        if (transform.position.y < MaxY)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+        }
+    }
+
+
+}
---
+++ Unity1/Assets/MagicCircle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircle : MonoBehaviour
+{
+
+    public IronBars IronBars;
+    public ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(ps!=null)
+        {
+            ps.Play();
+            Destroy(ps.transform.parent.gameObject, 2.0f);
+        }
+
+
+
+
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        IronBars.SendMessage("Lift");
+
+
+
+        //print(other.name+" is hit me");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Collision Enter");
+    }
+
+
+}
